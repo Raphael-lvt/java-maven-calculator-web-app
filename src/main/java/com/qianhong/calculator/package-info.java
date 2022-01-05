6792c21815414d94364a6d16a9ce14e2574ff975
unknown <louvet.raphael@gmail.com>
2022-01-05 15:14:35 +0100

    supression regle

--- src/main/java/com/qianhong/calculator/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* This package has non null parameters and is documented.
-**/
-@ParametersAreNonnullByDefault
-package com.qianhong.calculator;
-
-import javax.annotation.ParametersAreNonnullByDefault;
